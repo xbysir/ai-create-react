a07c2ed4de1f64dda18e882bd621a20c54721071
xby <2549621945@qq.com>
2025-03-18 11:58:16 +0800

    feat: 配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,18 @@ import UnoCSS from "unocss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    // 确保正确的资源引用路径
+    rollupOptions: {
+      output: {
+        chunkFileNames: "js/[name]-[hash].js",
+        entryFileNames: "js/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
   plugins: [UnoCSS(), react()],
 });
